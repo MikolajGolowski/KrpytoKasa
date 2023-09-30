d718fcc5e3452d1149f7e098be587c8daec99882
Mikołaj Golowski <GOLOMIK@hilti.com>
2023-09-30 14:03:28 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,5 @@
+# KRYPTO KASA
+
+## Lista endpointow i ich opis:
+1. Pobranie listy z nazwami wszystkich par (float/krypto)
+2. Lista wsz
---
+++ WeatherForecast.cs
@@ -0,0 +1,17 @@
+namespace KrpytoKasa
+{
+    public class WeatherForecast
+    {
+        public WeatherForecast()
+        {
+            
+        }
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
